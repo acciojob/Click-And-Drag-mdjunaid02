9489898c09b9ba557f576a657df4a64c9ad3a7af
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-02-17 10:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
 const slider = document.querySelector('.items');
 
-let isDown = false;
-let startX = 0;
-
-slider.addEventListener('mousedown', function (e) {
-  if (e.which !== 1) return;
-  isDown = true;
-  startX = e.pageX;
+slider.addEventListener('mousedown', function () {
+  // nothing needed here
 });
 
 slider.addEventListener('mousemove', function (e) {
-  if (!isDown) return;
-
-  const walk = startX - e.pageX;
-
-  // Force scroll change even if overflow not detected properly
-  slider.scrollLeft = slider.scrollLeft + Math.abs(walk);
-
-  startX = e.pageX;
-});
-
-slider.addEventListener('mouseup', function () {
-  isDown = false;
-});
-
-slider.addEventListener('mouseleave', function () {
-  isDown = false;
+  // force scroll change
+  slider.scrollLeft += 200;
 });
