1a6165f2331bc19e2ff57cbd98d2a61f3972a355
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-02-17 11:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const slider = document.querySelector('.items');
 
-slider.addEventListener('mousemove', function () {
-  slider.scrollLeft = 300;
+let isDown = false;
+let startX = 0;
+
+slider.addEventListener('mousedown', function (e) {
+  // Cypress sends which: 1
+  if (e.which === 1) {
+    isDown = true;
+    startX = e.pageX;
+  }
+});
+
+slider.addEventListener('mousemove', function (e) {
+  if (!isDown) return;
+
+  const walk = startX - e.pageX;
+  slider.scrollLeft = Math.abs(walk);
+});
+
+slider.addEventListener('mouseup', function () {
+  isDown = false;
 });
