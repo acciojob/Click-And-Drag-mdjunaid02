991d83de80c7b06d6773c747853dc53f20956410
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-02-17 10:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 const slider = document.querySelector('.items');
 
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active');
   startX = e.pageX;
   scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
 });
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
 
-  const walk = (e.pageX - startX);
-  slider.scrollLeft = scrollLeft - walk;
+  const walk = startX - e.pageX;
+  slider.scrollLeft = scrollLeft + walk;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
 });
